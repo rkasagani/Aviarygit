7e4c9dd9a7b18d16c37c7db1ab54ed52e8ddb6fe
rkasagani <renukasagani@gmail.com>
2014-07-07 16:51:33 +0530

    removed extra files

--- Aviary/Aviary/ViewController.h
+++ Aviary/Aviary/ViewController.h
@@ -1,5 +1,5 @@
 //
-//  AppDelegate.h
+//  ViewController.h
 //  Aviary
 //
 //  Created by saven on 7/7/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@interface ViewController : UIViewController
 
 @end
---
+++ Aviary/Aviary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Aviary
+//
+//  Created by saven on 7/7/14.
+//  Copyright (c) 2014 saven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
